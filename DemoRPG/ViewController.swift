16a338409d41ba8c6d4f3f9f9261550bd2c0c753
Jason Larsen <jason.larsen@gmail.com>
2016-08-13 21:40:23 -0600

    Initial Commit

---
+++ DemoRPG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoRPG
+//
+//  Created by Jason Larsen on 8/13/16.
+//  Copyright © 2016 Jason Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
